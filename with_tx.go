eafc17d1e9027be5766081fdbe71150f00adfba9
utgwkk <utagawakiki@gmail.com>
2024-09-28 12:03:19 +0900

    WithTx, WithTx2

---
+++ with_tx.go
@@ -0,0 +1,43 @@
+package sqlxext
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+func WithTx(ctx context.Context, db *sqlx.DB, f func(tx *sqlx.Tx) error) error {
+	tx, err := db.BeginTxx(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("failed to BeginTxx: %w", err)
+	}
+	defer tx.Rollback()
+
+	if err := f(tx); err != nil {
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("failed to Commit: %w", err)
+	}
+
+	return nil
+}
+
+func WithTx2[T any](ctx context.Context, db *sqlx.DB, f func(tx *sqlx.Tx) (T, error)) (T, error) {
+	var t T
+	f2 := func(tx *sqlx.Tx) error {
+		_t, err := f(tx)
+		if err != nil {
+			return err
+		}
+		t = _t
+		return nil
+	}
+
+	if err := WithTx(ctx, db, f2); err != nil {
+		return t, err
+	}
+	return t, nil
+}
